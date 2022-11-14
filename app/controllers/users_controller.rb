dd03bc651d2ea5fbf8e2e01d4cb6b6c63f60805a
Summer-Lu0208 <slu12@chicagobooth.edu>
2022-11-14 16:59:23 +0000

    final final

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,10 +9,20 @@ class UsersController < ApplicationController
 def show
 
 url_username=params.fetch("path_username")
- matching_usernames= User.where ({:username => url_username })
- @the_user=matching_usernames.at(0)
+ matching_users= User.where ({:username => url_username })
+ @the_user=matching_users.at(0)
 
   render({:template => "user_templates/show.html.erb"})
 end
+
+def create
+  user = User.new
+  user.username = params.fetch("input_username")
+  user.save
+  
+  redirect_to("/users/#{user.username}")
+end
+
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-
+  get("/", {:controller => "users", :action => "index"})
   get("/users", {:controller => "users", :action => "index"})
   get("/users/:path_username", {:controller => "users", :action => "show"})
+  get("/insert_user_record", { :controller => "users", :action => "create" })
   get("/photos",{:controller => "photos", :action => "index"})
-#the :path_username is the dynamic piece
+  get("/photos/:path_id",{:controller => "photos", :action => "show"})
+  get("/delete_photo/:toast_id",{:controller => "photos", :action => "baii"})
+  get("/insert_photo", {:controller => "photos", :action => "create"})
+  get("/update_photo/:modify_id", {:controller =>"photos", :action=>"update"})
+  get("/insert_comment_record", { :controller => "comments", :action => "create" })
+  #the :path_username is the dynamic piece
 end
